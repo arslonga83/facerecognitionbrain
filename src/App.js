fc9c711799a8702598b88fc670e2b342c3cfd9c1
arslonga83 <jlwood83@gmail.com>
2022-09-28 10:45:13 -0700

    add particles background

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
+import Rank from './components/rank/rank';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
-      {/* <Rank /> */}
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRecognition /> */}
     </div>
