76eb67d9fb03f210b8f58618ed4cb2d437c5c71b
arslonga83 <jlwood83@gmail.com>
2022-09-28 12:11:58 -0700

    listeners for input and button

--- src/App.js
+++ src/App.js
@@ -6,17 +6,34 @@ import Rank from './components/rank/rank';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
 import ParticlesBg from 'particles-bg'
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
   return (
     <div className="App">
       <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit = {this.onButtonSubmit} />
       {/*<FaceRecognition /> */}
     </div>
   );
+}
 }
 
 export default App;
