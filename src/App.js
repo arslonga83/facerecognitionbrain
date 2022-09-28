5dd91fd838bc0d07b95718dde09bc334a421bd4a
arslonga83 <jlwood83@gmail.com>
2022-09-28 14:47:15 -0700

    start setting up clarifai api and add image to page

--- src/App.js
+++ src/App.js
@@ -4,22 +4,42 @@ import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import Rank from './components/rank/rank';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
-import ParticlesBg from 'particles-bg'
+import FaceRecognition from './components/faceRecognition/faceRecognition';
+import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '94a47472f2ec45d39de77f86f4a3cd89'
+ });
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       input: '',
+      imageURL: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
     console.log('click');
+    this.setState({imageURL: this.state.input})
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(
+        function(response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        },
+        function(err) {
+
+        }
+      );
   }
 
   render() {
@@ -30,7 +50,7 @@ class App extends React.Component {
       <Logo />
       <Rank />
       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit = {this.onButtonSubmit} />
-      {/*<FaceRecognition /> */}
+      <FaceRecognition imageURL={this.state.imageURL} />
     </div>
   );
 }
