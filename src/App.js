df5a572ccdf7f5718c6b072059b751fd498f443b
arslonga83 <jlwood83@gmail.com>
2022-10-05 11:03:48 -0700

    connect to heroku server

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({imageURL: this.state.input})
-    fetch('http://localhost:3000/imageurl', {
+    fetch(' https://desolate-brook-36264.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -76,7 +76,7 @@ class App extends React.Component {
       .then(response => response.json()) 
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch(' https://desolate-brook-36264.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
