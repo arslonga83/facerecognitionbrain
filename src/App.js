6c6e105f20bd6728a4eaf5dfe03c82256a314970
arslonga83 <jlwood83@gmail.com>
2022-10-05 12:21:42 -0700

    fix port connection to image

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends React.Component {
       .then(response => response.json()) 
       .then(response => {
         if (response) {
-          fetch(' https://desolate-brook-36264.herokuapp.com:3000/image', {
+          fetch(' https://desolate-brook-36264.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
